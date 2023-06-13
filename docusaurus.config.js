01c62cc6cf3e92071b8deb2cef8ba28e58a68a02
NikBel3476 <nikita.belkin3476@gmail.com>
2023-06-13 17:56:36 +0400

    chore: remote deployment branch from config

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -11,7 +11,7 @@ const config = {
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
-  url: 'https://NikBel3476.github.io/bim-json-docs',
+  url: 'https://NikBel3476.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/bim-json-docs/',
@@ -20,7 +20,6 @@ const config = {
   // If you aren't using GitHub pages, you don't need these.
   organizationName: 'NikBel3476', // Usually your GitHub org/user name.
   projectName: 'bim-json-docs', // Usually your repo name.
-  deploymentBranch: 'master',
   trailingSlash: true,
 
   onBrokenLinks: 'throw',
